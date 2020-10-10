5e497d30533fac0d10033c0be4de564732df52e9
MounikaVojjala <mounikavojjala@gmail.com>
2020-10-10 20:35:25 +0000

    for-challange

---
+++ new.js
@@ -0,0 +1,11 @@
+for(let i = 100; i <= 200; i++){
+  if(i%3 === 0 && i%4 === 0){
+    console.log("LoopyLighthouse");
+  }
+  if(i%3 === 0){
+    console.log("Loopy");
+  }
+  if(i%4 === 0){
+    console.log("Lighthouse");
+  }
+}
