06c14abbbb2d15361c79ef2612e2288811dc928a
MounikaVojjala <mounikavojjala@gmail.com>
2020-10-12 19:18:19 +0000

    last index of given number

---
+++ lastIndex.js
@@ -0,0 +1,10 @@
+function lastIndexOf(arr, val) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(lastIndexOf([0, 1, 4, 1, 2], 1));
