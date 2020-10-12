634dfe0e2f14d2a7d29bb0461bacc9c862f57e8a
MounikaVojjala <mounikavojjala@gmail.com>
2020-10-12 18:27:04 +0000

    changes done

--- chooseStations.js
+++ chooseStations.js
@@ -8,17 +8,17 @@ const stations = [
 
 
 
-const chooseStations = function(stations){
-const goodStations = [];
-for(const station of stations){
-let capacity = station[1];
-let type = station[2];
-if(capacity >= 20 && (type === "school" || type === "community centre")){
-goodStations.push(station[0]);
- 
+const chooseStations = function (stations) {
+  const goodStations = [];
+  for (const station of stations) {
+    let capacity = station[1];
+    let type = station[2];
+    if (capacity >= 20 && (type === "school" || type === "community centre")) {
+      goodStations.push(station[0]);
+
+    }
   }
-}
-return goodStations;
+  return goodStations;
 }
 
 
